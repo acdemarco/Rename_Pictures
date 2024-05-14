6c71778cea86cb33000c5e88a709fc7272dab174
Andres DeMarco <acdemarco@yahoo.com>
2024-05-14 12:06:27 -0400

    initial Version 1.5

--- Rename_All.py
+++ Rename_All.py
@@ -3,13 +3,20 @@ from stat import *
 from PIL import Image
 from datetime import datetime
 from PIL.ExifTags import TAGS
-import exifread
+from hachoir.parser import createParser
+from hachoir.metadata import extractMetadata
+#import exifread
 import logging
+from pillow_heif import register_heif_opener
+
+register_heif_opener()
+
+
 
 logging.basicConfig(filename='Rename_All.log', filemode='w', format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',level=logging.INFO)
 file_num = 0  # this is the start number for the new names
 
-def walktree(top, ext, fileNum, callback):
+def walktree(top, ext, fileNum, destfolder, callback):
     """
     recursively descend the directory tree rooted at top,
     calling the callback function for each regular file
@@ -30,34 +37,92 @@ def walktree(top, ext, fileNum, callback):
             walktree(pathname, ext, callback)
         elif S_ISREG(mode):
             # It's a file, call the callback function
-            callback(pathname, f, file_num, top, ext)
+            callback(pathname, f, file_num, top, ext, destfolder)
         else:
             # Unknown file type, print a message
             logging.info('Skipping %s' % pathname)
 
 
-def visitfile(file, f, fnum, folder, fileext):
+
+def get_date_taken(image_path):
+    try:
+        with Image.open(image_path) as img:
+            #if img.has_exif:
+            exif_data = img.getexif()
+            if exif_data:
+                for tag, value in exif_data.items():
+                    tag_name = TAGS.get(tag, tag)
+                    if tag_name == 'DateTime':
+                        return value
+            return None
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+
+def get_video_creation_date(video_path):
+    with createParser(video_path) as parser:
+    #parser = createParser(video_path)
+        if not parser:
+            print("Error: Unable to parse file.")
+            return None
+
+        metadata = extractMetadata(parser)
+        if not metadata:
+           print("Error: Unable to extract metadata.")
+           return None
+
+        for line in metadata.exportPlaintext():
+            if "Creation date" in line:
+                date_string = line.split(" ")[3].strip() + ' ' + line.split(" ")[4].strip()
+                try:
+                    date_taken = datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
+                    return date_taken
+                except ValueError:
+                    print("Error: Unable to parse creation date.")
+                    return None
+
+    print("Creation date not found in metadata.")
+    return None
+
+
+def visitfile(file, f, fnum, folder, fileext, newfolder):
     logging.info('visiting ' + file + f + str(fnum) + folder + fileext)
     has_info = 0
     img_open = 0
     try:
         logging.info("Before image open")
-        with Image.open(file) as img:
-            # img = Image.open(file)
-            d = img.getexif()
-            logging.info(d)
-            img_open = 1
-        for key, value in d.items():
-            if key == 306:
-                date_taken = value
-                img_time = datetime.strptime(date_taken, '%Y:%m:%d %H:%M:%S')
-                str_time = str(datetime.strftime(img_time, '%Y_%m_%d'))
-                if str_time is not None:
-                    has_info = 1
-        logging.info(str_time)
-        logging.info(type(str_time))
+        if fileext == 'mov' or fileext =='MOV' or fileext == 'mp4' or fileext == 'MP4':
+            date_taken_result = get_video_creation_date(file)
+            logging.info(f"This is the date taken value:{date_taken_result}")
+            #img_time = datetime.strptime(date_taken_result, '%Y-%m-%d %H:%M:%S')
+            str_time = str(datetime.strftime(date_taken_result, '%Y_%m_%d'))
+        else:
+            date_taken_result = get_date_taken(file)
+            logging.info(f"This is the date taken value:{date_taken_result}")
+            img_time = datetime.strptime(date_taken_result, '%Y:%m:%d %H:%M:%S')
+            str_time = str(datetime.strftime(img_time, '%Y_%m_%d'))
 
-        logging.info("before image close 1")
+        logging.info(f"This is the date taken value converted:{str_time}")
+        if str_time is not None:
+            has_info = 1
+        #print(datevar)
+                
+        # with Image.open(file) as img:
+        #     # img = Image.open(file)
+        #     d = img.getexif()
+        #     logging.info(d)
+        #     img_open = 1
+        # for key, value in d.items():
+        #     if key == 306:
+        #         date_taken = value
+        #         img_time = datetime.strptime(date_taken, '%Y:%m:%d %H:%M:%S')
+        #         str_time = str(datetime.strftime(img_time, '%Y_%m_%d'))
+        #         if str_time is not None:
+        #             has_info = 1
+        # logging.info(str_time)
+        # logging.info(type(str_time))
+        #logging.info("before image close 1")
         # img.close()
  #   except UnboundLocalError as U_error:
  #       print(U_error)
@@ -65,16 +130,16 @@ def visitfile(file, f, fnum, folder, fileext):
  #           img.close()
     except:
         logging.info("Image does not contain information.")
+        tmp = time.strptime((time.ctime(os.path.getmtime(file))))  # the modification date
+        os.rename(file, newfolder + "/" + fileext + "_" + str(tmp[0]) + "_" + str(tmp[1]) + "_" + str(fnum) + "." + fileext)
+        logging.info("new file: " + newfolder + "/" + fileext + "_" + str(tmp[0]) + "_" + str(tmp[1]) + "_" + str(fnum) + "." + fileext)
         # if img_open == 0:
             # img.close()
 
-    tmp = time.strptime((time.ctime(os.path.getmtime(file))))  # the modification date
+
     if has_info == 1:
-        os.rename(file, folder + "/" + fileext + "_" + str_time + "_" + str(fnum) + "." + fileext)
-        logging.info("new file: " + folder + "/" + fileext + "_" + str_time + "_" + str(fnum) + "." + fileext)
-    else:
-        os.rename(file, folder + "/" + fileext + "_" + str(tmp[0]) + str(tmp[1]) + "_" + str(fnum) + "." + fileext)
-        logging.info("new file: " + folder + "/" + fileext + "_" + str(tmp[0]) + str(tmp[1]) + "_" + str(fnum) + "." + fileext)
+         os.rename(file, newfolder + "/" + fileext + "_" + str_time + "_" + str(fnum) + "." + fileext)
+         logging.info("new file: " + newfolder + "/" + fileext + "_" + str_time + "_" + str(fnum) + "." + fileext)
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -5,11 +5,14 @@ import PySimpleGUI as sg
 label = sg.Text("Source Folder: ")
 label2 = sg.Text("Rename Number: ")
 label3 = sg.Text("Target extension (jpeg,gif, etc.): ")
+label4 = sg.Text("Destination Folder: ")
 
 Input_Source = sg.InputText(tooltip="Enter Source Folder.", key='Source_Folder')
+Input_Destf = sg.InputText(tooltip="Enter Destination Folder.", key='Dest_Folder')
 Input_Dest = sg.InputText(tooltip="Enter Starting number for rename.", key='Rename_number')
 Input_Ext = sg.InputText(tooltip="Enter Target Ext.", key='Source_Ext')
 choose_folder = sg.FolderBrowse("Folder...")
+choose_folder2 = sg.FolderBrowse("Folder...")
 
 run_button = sg.Button("Run")
 exit_button = sg.Button("Exit")
@@ -17,6 +20,7 @@ exit_button = sg.Button("Exit")
 window = sg.Window("Rename Image Files",
                    layout=[
                        [label, Input_Source, choose_folder],
+                       [label4,Input_Destf, choose_folder2],
                        [label2], [Input_Dest], [label3], [Input_Ext],
                        [exit_button, run_button]
                         ], font=('Helvetica', 20))
@@ -28,10 +32,13 @@ while True:
         case "Run":
             print("Run")
             SourceFolder = value['Source_Folder']
+            DestFolder = value['Dest_Folder']
             StartNum = int(value['Rename_number'])
             DestExt = value['Source_Ext']
-            print(SourceFolder, StartNum, DestExt)
-            RA.walktree(SourceFolder, DestExt, StartNum, RA.visitfile)
+            print(SourceFolder, DestFolder,StartNum, DestExt)
+            RA.walktree(SourceFolder, DestExt, StartNum, DestFolder ,RA.visitfile)
+            sg.popup("Program Done", "The program has completed its task.")
+            break
         case "Exit":
             # exit(0)
             break
