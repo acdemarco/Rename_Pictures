42432f445f82ebf57cbe5c0f630a1df0b2713ebc
Andres DeMarco <acdemarco@yahoo.com>
2024-09-29 12:32:37 -0400

    initial Version 1.5

--- Rename_All.py
+++ Rename_All.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from PIL.ExifTags import TAGS
 from hachoir.parser import createParser
 from hachoir.metadata import extractMetadata
+from pymediainfo import MediaInfo
 #import exifread
 import logging
 from pillow_heif import register_heif_opener
@@ -60,6 +61,31 @@ def get_date_taken(image_path):
         return None
 
 
+def get_video_creation_date2(video_path):
+        media_info = MediaInfo.parse(video_path)
+        # Loop through tracks and print modification date
+        for track in media_info.tracks:
+            if track.track_type == "General":
+
+                MyDate = track.file_last_modification_date
+                dt_obj = datetime.strptime(MyDate, "%Y-%m-%d %H:%M:%S.%f UTC")
+                # Format the datetime object to the desired format without milliseconds or UTC
+                formatted_timestamp = dt_obj.strftime("%Y-%m-%d %H:%M:%S")
+                MyDate_datetime: str = formatted_timestamp
+                #date_string = MyDate.split(" ")[3].strip() + ' ' + MyDate.split(" ")[4].strip()
+                #print(date_string)
+                try:
+                    #date_taken = datetime.strptime(MyDate, "%Y-%m-%d %H:%M:%S")
+                    date_taken = MyDate_datetime
+                    #print(date_taken)
+                    return date_taken
+                except ValueError:
+                    print("Error: Unable to parse creation date.")
+                    return None
+
+        print("Creation date not found in metadata.")
+        return None
+
 def get_video_creation_date(video_path):
     with createParser(video_path) as parser:
     #parser = createParser(video_path)
@@ -73,7 +99,8 @@ def get_video_creation_date(video_path):
            return None
 
         for line in metadata.exportPlaintext():
-            if "Creation date" in line:
+            #if "Creation date" in line:
+            if "Last modification" in line:
                 date_string = line.split(" ")[3].strip() + ' ' + line.split(" ")[4].strip()
                 try:
                     date_taken = datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
@@ -93,10 +120,15 @@ def visitfile(file, f, fnum, folder, fileext, newfolder):
     try:
         logging.info("Before image open")
         if fileext == 'mov' or fileext =='MOV' or fileext == 'mp4' or fileext == 'MP4':
-            date_taken_result = get_video_creation_date(file)
+            date_taken_result = get_video_creation_date2(file)
             logging.info(f"This is the date taken value:{date_taken_result}")
             #img_time = datetime.strptime(date_taken_result, '%Y-%m-%d %H:%M:%S')
-            str_time = str(datetime.strftime(date_taken_result, '%Y_%m_%d'))
+            # Parse the string into a datetime object
+            dt_obj = datetime.strptime(date_taken_result, '%Y-%m-%d %H:%M:%S')
+
+            # Format the datetime object to the desired format '%Y_%m_%d'
+            str_time = dt_obj.strftime('%Y_%m_%d')
+            #str_time = str(datetime.strftime(date_taken_result, '%Y_%m_%d'))
         else:
             date_taken_result = get_date_taken(file)
             logging.info(f"This is the date taken value:{date_taken_result}")
